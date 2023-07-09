87c0de07dd7375bb0b7ede2c6c34efb5ed0f9f18
Sardorbek <sardorbekaminjonov2001@mail.ru>
2023-07-09 23:50:17 +0500

    Protect db

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,11 +3,15 @@ import { Room } from "../model/Room";
 import { User } from "../model/User";
 import { Organisation } from "../model/Organisation";
 
+const POSTGRES_USERNAME = process.env.POSTGRES_USERNAME;
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+const POSTGRES_DATABASE = process.env.POSTGRES_DATABASE;
+
 export const sequelize = new Sequelize({
-  database: "super-waiter",
+  database: POSTGRES_DATABASE,
   dialect: "postgres",
-  username: "sardorbek",
-  password: "admin",
+  username: POSTGRES_USERNAME,
+  password: POSTGRES_PASSWORD,
   storage: ":memory:",
   host: "localhost",
   // host: 'host.docker.internal',
