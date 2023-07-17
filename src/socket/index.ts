257b10f6261aea1727e8674ac61a16f99847d3df
Sardorbek <sardorbekaminjonov2001@mail.ru>
2023-07-17 13:00:32 +0500

    Extend socket

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,12 +1,39 @@
-import { IncomingMessage, ServerResponse } from "http";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
 
-type SocketType = Server<typeof IncomingMessage, typeof ServerResponse>;
+// Define custom event names
+type CustomEvents = {
+  "user-joined": (userId: string) => void;
+  "send-message": (message: string, userId: string) => void;
+  "receive-message": (message: string) => void;
+};
+
+type SocketType = Server<CustomEvents, CustomEvents, DefaultEventsMap>;
+
+const connectedUsers: { [userId: string]: Socket } = {};
 
 export const useSocket = (io: SocketType) => {
   io.on("connection", (socket) => {
-    console.log("a user connected");
+    connectedUsers[socket.id] = socket;
 
+    // Handle custom event "user-joined"
+    socket.on("user-joined", (userId: string) => {
+      // Broadcast the event to all connected users except the sender
+      socket.broadcast.emit("user-joined", userId);
+    });
+
+    // Handle custom event "send-message"
+    socket.on("send-message", (message: string, userId: string) => {
+      // Get the recipient's socket by their user ID
+      const recipientSocket = connectedUsers[userId];
+
+      // If the recipient socket exists, emit the event to the recipient only
+      if (recipientSocket) {
+        recipientSocket.emit("receive-message", message);
+      }
+    });
+
+    // Disconnect client
     socket.on("disconnect", () => {
       console.log("user disconnected");
     });
